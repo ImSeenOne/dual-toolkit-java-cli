1f9ba356f65f7167c9dd96df66bd99a5612ffd08
Cristian <cristianramirezdev@hotmail.com>
2024-05-27 01:51:53 -0600

    Deleted compiling script, it's already on the mvn clean compile command

--- compile.sh
+++
@@ -1,3 +0,0 @@
-echo "Compiling Dual source..."
-javac -d target/classes -sourcepath src/main/java -classpath target/classes src/main/java/com/caveman/dual/DualApplication.java
-echo "Dual source compiled"
